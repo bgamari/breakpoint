cd496d6b0b35ab8d26b28a401dacddeba1915826
Ben Gamari <ben@smart-cactus.org>
2017-06-24 16:53:41 -0400

    Add logging bits

--- System/Breakpoint.hs
+++ System/Breakpoint.hs
@@ -1,11 +1,20 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE UnboxedTuples #-}
 
 module System.Breakpoint
     ( breakOnEval
     , breakpoint
+    , logWord
+    , logPointer
+    , logThreadId
     ) where
 
 import System.IO.Unsafe
+import GHC.IO
+import GHC.Word
+import GHC.Int
+import GHC.Prim
 
 foreign import ccall unsafe "c_breakpoint" breakpoint :: IO ()
 
@@ -13,3 +22,11 @@ breakOnEval :: a -> a
 breakOnEval x = unsafePerformIO $ do
     breakpoint
     return x
+
+logPointer :: a -> IO ()
+logPointer x = do
+    a <- IO $ \s -> case anyToAddr# x s of (# s', addr #) -> (# s', fromIntegral (I# (addr2Int# addr)) #)
+    logWord a
+
+foreign import ccall unsafe "c_log_tid" logThreadId :: IO ()
+foreign import ccall unsafe "c_log_word" logWord :: Word -> IO ()
--- cbits/breakpoint.c
+++ cbits/breakpoint.c
@@ -1,3 +1,27 @@
+#include <stdint.h>
+#include <pthread.h>
+
+#define LOG_SIZE 4096
+
+volatile uint64_t word_log[LOG_SIZE];
+volatile int log_idx = 0;
+volatile int lock = 0;
+
+void c_log_word(uint64_t x) {
+  while (__sync_lock_test_and_set(&lock, 1))
+      while (lock);
+  word_log[log_idx] = x;
+  log_idx = (log_idx + 1) % LOG_SIZE;
+  lock = 0;
+}
+
 void c_breakpoint() {
   __asm__("int $3");
 }
+
+void c_log_tid() {
+  pthread_t tid = pthread_self();
+  c_log_word(tid);
+}
+
+struct thing { uint64_t a, b, c, d; };
